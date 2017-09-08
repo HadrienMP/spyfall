8239eb67c69f968cdf22cbdf81b268bf1cf29432
HadrienMP <public@hadrienmp.fr>
2017-09-08 19:30:29 +0200

    Improve exceptions, attempt

--- src/main/kotlin/fr/hadrienmp/spyfall/adapters/ins/web/main.kt
+++ src/main/kotlin/fr/hadrienmp/spyfall/adapters/ins/web/main.kt
@@ -4,22 +4,30 @@ import fr.hadrienmp.spyfall.adapters.ins.MutableGame
 import fr.hadrienmp.spyfall.adapters.outs.HardCodedLocations
 import fr.hadrienmp.spyfall.domain.game
 import io.javalin.Javalin
+import io.javalin.LogLevel
 
 
 fun main(args: Array<String>) {
+    val app = Javalin.create()
+            .port(8080)
+            .requestLogLevel(LogLevel.STANDARD)
+    exceptionMapping(app)
+    routes(app)
+    app.start()
+}
+
+private fun routes(app: Javalin) {
     val locations = HardCodedLocations()
     val game = MutableGame(game(locations))
 
     val gameCtrl = GameCtrl(game)
-    val player = PlayerCtrl(game)
+    val playerCtrl = PlayerCtrl(game)
     val locationsCtrl = LocationsCtrl(locations)
 
-    val app = Javalin.start(8080)
     app.get("/") { it.redirect("/game") }
     app.get("/game", gameCtrl::display)
     app.get("/game/start", gameCtrl::start)
-    app.post("/player/register", player::register)
-    app.get("/player/card", player::displayCard)
+    app.post("/player/register", playerCtrl::register)
+    app.get("/player/card", playerCtrl::displayCard)
     app.get("/locations", locationsCtrl::display)
 }
-
