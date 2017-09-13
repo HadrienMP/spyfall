71ebf6526dbd49d5af849b2a92cf26d78f5add69
Hadrien Mens-Pellen <hadrien.menspellen@darty.fr>
2017-09-13 15:21:19 +0200

    Mise en place des vrais lieux

--- src/main/kotlin/fr/hadrienmp/spyfall/lib/HardCodedLocations.kt
+++ src/main/kotlin/fr/hadrienmp/spyfall/lib/HardCodedLocations.kt
@@ -4,7 +4,38 @@ import fr.hadrienmp.spyfall.domain.Locations
 import java.util.*
 
 class HardCodedLocations: Locations {
-    private val locations = listOf("Beach", "Office", "Airport")
+    private val locations = listOf(
+            "Ambassade",
+            "Avion",
+            "Banque",
+            "Base militaire",
+            "Bateau pirate",
+            "Boite de nuit",
+            "Carnaval",
+            "Casino",
+            "Cirque",
+            "Croisades",
+            "École",
+            "Fête d'entreprise",
+            "Garage",
+            "Hôpital",
+            "Hôtel",
+            "Paquebot",
+            "Parc d'attraction",
+            "Plage",
+            "Poste de police",
+            "Restaurant",
+            "Sous-marin",
+            "Station polaire",
+            "Station spatiale",
+            "Studio de cinéma",
+            "Supermarché",
+            "Centre de thalasso",
+            "Théatre",
+            "Train de voyageurs",
+            "Université",
+            "Zoo"
+    )
 
     override fun random() = locations.randomPick()
 
